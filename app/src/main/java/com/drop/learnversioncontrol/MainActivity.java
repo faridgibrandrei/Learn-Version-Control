afc447f932bce991c1fad817ce84c51f9e0facf9
Farid Gibran <faridgibrandrei@Farids-MacBook-Pro.local>
2021-03-03 19:47:05 +0700

    added operate func

--- app/src/main/java/com/drop/learnversioncontrol/MainActivity.java
+++ app/src/main/java/com/drop/learnversioncontrol/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String OPERATION_PLUS = "operation_plus";
+    public static final String OPERATION_MINUS = "operation_minus";
+    public static final String OPERATION_MULTIPLE = "operation_multiple";
+    public static final String OPERATION_DIVIDE = "operation_divide";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
 
         String messageFromUser1 = "third message by user1";
         String messageFromUser2 = "This is the First message from user2";
+    }
+
+    private int operate(int a, int b, String operation) {
+        int result = 0;
+
+        switch (operation) {
+            case OPERATION_PLUS:
+                result = a + b;
+                break;
+            case OPERATION_MINUS:
+                result = a - b;
+                break;
+            case OPERATION_MULTIPLE:
+                result = a * b;
+                break;
+            case OPERATION_DIVIDE:
+                result = a / b;
+                break;
+            default:
+                break;
+        }
+
+        return result;
     }
 }
