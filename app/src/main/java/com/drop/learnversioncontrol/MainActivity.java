2f64e56cb374285ab9fb610b95db12446d269159
faridgibrandrei <farid.gibran@vostra.co.id>
2021-03-04 10:51:38 +0700

    added toast function

--- app/src/main/java/com/drop/learnversioncontrol/MainActivity.java
+++ app/src/main/java/com/drop/learnversioncontrol/MainActivity.java
@@ -1,6 +1,8 @@
 package com.drop.learnversioncontrol;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
         String messageFromUser1 = "third message by user1";
         String messageFromUser2 = "This is the First message from user2";
+    }
+
+    private void toast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
